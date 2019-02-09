141dad3289d055a7414d9b32e71292c8d2ce9a35
Samyak Jain <samyak.jn11@gmail.com>
2019-02-09 21:10:24 +0530

    Basic hello world.

---
+++ first.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class check 
+{
+    public static void main (String args[])
+            {
+                        System.out.println("Hello world");
+                        System.out.println("lEARNNG BASICS OF java");
+
+            }
+}
+                                                        
